475d26729cdc3e5d73dba7551572d4e0f56259ff
Irving2355 <it235597@gmail.com>
2026-03-27 17:32:59 -0600

    Clase 27-03-2026

--- Parcial 2/DosBOX/imc.asm
+++ Parcial 2/DosBOX/imc.asm
@@ -34,7 +34,7 @@ ParseBufferToReal proc
     ;reiniciar variables
     mov word ptr intPart, 0
     mov word ptr fracPart, 0
-    mov word ptr fracDiv, 0
+    mov word ptr fracDiv, 1
     mov byte ptr seenDot, 0 
 
     mov cl, [si+1]
@@ -109,6 +109,47 @@ ParseBufferToReal proc
     ret
 ParseBufferToReal endp
 
+PrintAX proc
+    ;push ax
+    push bx
+    push cx
+    push dx
+
+    cmp ax, 0
+    jne convertirNumero
+
+    mov dl, '0'
+    mov ah, 02h
+    int 21h
+    jmp finPrint
+
+    convertirNumero:
+    mov bx, 10
+    xor cx, cx
+
+    ciclo1:
+    xor dx, dx
+    div bx
+    push dx
+    inc cx
+    cmp ax, 0
+    jne ciclo1
+
+    ciclo2:
+    pop dx
+    add dl, '0'
+    mov ah, 02h
+    int 21h
+    loop ciclo2
+
+    finPrint:
+    pop dx
+    pop cx
+    pop bx
+    ;pop ax
+    ret
+PrintAX endp
+
 main:
     mov ax, @data 
     mov ds, ax
@@ -142,4 +183,43 @@ main:
 
     fld numParsed
     fstp altura
+
+    ;calcular imc
+    fld altura
+    fmul altura
+    fstp altura2
+
+    fld peso 
+    fdiv altura2
+    fstp imc 
+
+    ;mostrar el imc
+    mov ah, 09h
+    lea dx, msgRes
+    int 21h
+
+    ;descomoner imc
+    ;28.88*100 = 2888
+    fld imc
+    fimul cien 
+    fistp imc100
+
+    mov ax, imc100
+    xor dx, dx
+    mov bx, 100
+    div bx 
+    ;ax entera
+    ;dx fraccion 
+
+    call PrintAX
+
+    mov dl, '.'
+    mov ah, 02h
+    int 21h
+
+    mov ax, dx
+    call PrintAX
+
+    mov ah, 4Ch
+    int 21h
 end main
