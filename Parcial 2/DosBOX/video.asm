27f206498f5f866cbd77b82f84c1a830a58f706c
Irving2355 <it235597@gmail.com>
2026-04-07 17:34:44 -0600

    Clase 07-04-2026

--- Parcial 2/DosBOX/video.asm
+++ Parcial 2/DosBOX/video.asm
@@ -21,6 +21,55 @@ msgMostrar db 'Tu nombre fue: $'
 msgLimpio db 'El buffer se limpio$'
 msgRepetir db 'Otra vez (s/n): $'  
 .code 
+
+LimpiarPantalla proc
+    ;ah = 06h y al = 00h
+    mov ah, 06h
+    mov al, 00h
+    mov bh, 07h
+    mov ch, 00h
+    mov cl, 00h
+    mov dh, 24h
+    mov dl, 79h
+    int 10h
+
+    mov dh, 0
+    mov dl, 0
+    call PosicionarCursor
+    ret 
+LimpiarPantalla endp
+
+PosicionarCursor proc
+    ;dh filas y dl las columnas
+    mov ah, 02h
+    mov bh, 00h
+    int 10h
+    ret
+PosicionarCursor endp
+
+ImprimirColor proc
+    siguiente_caracter:
+    ;al = [si++]
+    lodsb ;al = [si] automaticamente aumenta si
+    cmp al, '$'
+    je fin_imprimir_color
+
+    ;ah=09h caracter atributo
+    mov ah, 09h
+    mov bh, 00h
+    mov cx, 1
+    int 10h
+
+    ;movimiento del cursor hacia la derecha
+    mov ah, 0Eh
+    mov bh, 00h
+    int 10h
+
+    jmp siguiente_caracter 
+    fin_imprimir_color:
+    ret
+ImprimirColor endp
+
 main:
     ;iniciar el ds
     mov ax,@data 
