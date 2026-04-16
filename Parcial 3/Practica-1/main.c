256815462b25eb2ac9edab443cdadd7154b3969d
Irving2355 <it235597@gmail.com>
2026-04-16 17:46:17 -0600

    Clase 16-04-2026

--- Parcial 3/Practica-1/main.c
+++ Parcial 3/Practica-1/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int suma_arreglo(const int* arr, int n);
 
 int main(){
-    int datos[] = {1,2,3,4,5,6,7,8,9};
+    int datos[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
     int n = sizeof(datos) / sizeof(int);
 
@@ -15,5 +16,6 @@ int main(){
     }
 
     printf("\nSuma total = %d\n", resultado);
+    system("pause");
     return 0;
 }
